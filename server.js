6fc97e6ef239d2c5ad4561dd16cacc6a649b1340
Adeeb Jamil <154850969+adeebjamil@users.noreply.github.com>
2025-06-30 17:37:47 +0530

    up

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://frontend-hospitality.vercel.app',
+  origin: ['https://frontend-hospitality.vercel.app', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
